15be2583702ceac35268e41f1315b9fe0230bd56
tisfeng <tisfeng@gmail.com>
2022-05-26 21:46:05 +0800

    feat: modify request error prompt

--- src/easydict.tsx
+++ src/easydict.tsx
@@ -362,16 +362,17 @@ export default function () {
 
     return (
       <List.Item
-        title={`Sorry! We have some problems..`}
-        subtitle={`code: ${fetchResultStateCode}`}
+        title={`Sorry, it seems that there is something wrong with the request service.`}
+        accessories={[{ text: `Error code: ${fetchResultStateCode}` }]}
         icon={{ source: Icon.XmarkCircle, tintColor: Color.Red }}
         actions={
           <ActionPanel>
             <Action.OpenInBrowser
-              title="Help"
+              title="See Error Code Meaning"
               icon={Icon.QuestionMark}
-              url="https://github.com/Haojen/raycast-Parrot#error-code-information"
+              url="https://ai.youdao.com/DOCSIRMA/html/%E8%87%AA%E7%84%B6%E8%AF%AD%E8%A8%80%E7%BF%BB%E8%AF%91/API%E6%96%87%E6%A1%A3/%E6%96%87%E6%9C%AC%E7%BF%BB%E8%AF%91%E6%9C%8D%E5%8A%A1/%E6%96%87%E6%9C%AC%E7%BF%BB%E8%AF%91%E6%9C%8D%E5%8A%A1-API%E6%96%87%E6%A1%A3.html#section-11"
             />
+            <ActionFeedback />
           </ActionPanel>
         }
       />
