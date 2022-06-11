05a6a34646df1ded2f708e4292266dfbb1af5bc7
tisfeng <tisfeng@gmail.com>
2022-06-11 13:14:35 +0800

    refactor: bing crawl parse

--- src/bing.ts
+++ src/bing.ts
@@ -20,46 +20,71 @@ $('li[class=orange]').html()
  */
 
 export function bingTranslate(word: string) {
+  // https://cn.bing.com/dict/search?q=good
   const queryWordUrl = `https://cn.bing.com/dict/search?q=${encodeURI(word)}`;
 
   axios.get(queryWordUrl).then((response) => {
-    const $ = cheerio.load(response.data);
+    console.warn(
+      `//=================== Bing Translate: ${word} ===================//`
+    );
 
-    const pronounceText = $(".hd_p1_1>.hd_prUS").text();
-    console.warn(`pronounce: ${pronounceText}`);
+    const html = response.data;
+    const [phonetic, phoneticUrl] = parsePhonetic(html);
 
-    const pronounceElement = $(".hd_p1_1>.hd_tf:first");
-    console.log(`pronounceElement: ${pronounceElement}`);
+    parseExplains(html);
+    parseForms(html);
 
-    let data = [];
-    for (const element of $(".qdef>ul>li")) {
-      const part = $(element).find(".pos").text();
-      const meam = $(element).find(".def").text();
-      const partMean = `${part} ${meam}`;
-
-      data.push(partMean);
-      console.warn(partMean);
-    }
-
-    const fomrs = $(".hd_div1>.hd_if").text();
-    console.warn(`forms: ${fomrs}`);
-
-    let title = $(".hd_p1_1").find(".hd_tf").first().attr("class");
-    console.warn(`title: ${title}`);
-
-    // onclick = "javascript:BilingualDict.Click(this,'https://dictionary.blob.core.chinacloudapi.cn/media/audio/tom/8e/00/8E00A7C3C07F3A1E7B6706E266B8FC3B.mp3','akicon.png',false,'dictionaryvoiceid')"
-
-    const onclick = $(".bigaud").first().attr("onclick");
-    console.warn(`onclick: ${onclick}`);
-
-    if (onclick) {
-      const url = onclick.split("'")[1];
-      console.warn(`url: ${url}`);
-
+    if (phoneticUrl) {
       const audioPath = getWordAudioPath(word);
-      downloadWordAudio(url, audioPath, () => {
+      downloadWordAudio(phoneticUrl, audioPath, () => {
         playAudio(audioPath);
       });
     }
   });
+}
+
+// parse word pronounce from html
+export function parsePhonetic(html: string) {
+  const $ = cheerio.load(html);
+
+  let phonetic;
+  const pronounceText = $(".hd_p1_1>.hd_prUS").text();
+  if (pronounceText) {
+    phonetic = pronounceText.split("[")[1].split("]")[0];
+    console.warn(`phonetic: ${phonetic}`);
+  }
+
+  // onclick = "javascript:BilingualDict.Click(this,'https://dictionary.blob.core.chinacloudapi.cn/media/audio/tom/8e/00/8E00A7C3C07F3A1E7B6706E266B8FC3B.mp3','akicon.png',false,'dictionaryvoiceid')"
+
+  const onclick = $(".bigaud").first().attr("onclick");
+  let phoneticUrl;
+  if (onclick) {
+    phoneticUrl = onclick.split("'")[1];
+  }
+
+  return [phonetic, phoneticUrl];
+}
+
+// parse word expains from html
+export function parseExplains(html: string) {
+  const $ = cheerio.load(html);
+
+  let data = [];
+  for (const element of $(".qdef>ul>li")) {
+    const part = $(element).find(".pos").text();
+    const meam = $(element).find(".def").text();
+    const partMean = `${part} ${meam}`;
+
+    data.push(partMean);
+    console.warn(partMean);
+  }
+  return data;
+}
+
+// parse word forms from html
+export function parseForms(html: string) {
+  const $ = cheerio.load(html);
+  const fomrs = $(".hd_div1>.hd_if").text();
+  console.warn(`forms: ${fomrs}`);
+  return fomrs;
 }
