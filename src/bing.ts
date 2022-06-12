23cd1ddc02b2a77d653b7275649239f125763eb4
tisfeng <tisfeng@gmail.com>
2022-06-12 23:35:05 +0800

    feat: add bing paraphrase

--- src/bing.ts
+++ src/bing.ts
@@ -3,7 +3,6 @@ import axios from "axios";
 import { downloadWordAudio, getWordAudioPath, playAudio } from "./audio";
 
 export function bingTranslate(word: string) {
-  // https://cn.bing.com/dict/search?q=good
   const queryWordUrl = `https://cn.bing.com/dict/search?q=${encodeURI(word)}`;
 
   axios.get(queryWordUrl).then((response) => {
@@ -17,6 +16,8 @@ export function bingTranslate(word: string) {
     parseForms(html);
     parsePhrase(html);
 
+    parseParaphrase(html);
+
     const audioUrl = parseAudioUrl(html);
     if (audioUrl) {
       const audioPath = getWordAudioPath(word);
@@ -27,12 +28,39 @@ export function bingTranslate(word: string) {
   });
 }
 
+export function parseParaphrase(html: string) {
+  const $ = cheerio.load(html);
+  /**
+    <meta name="description" content="必应词典为您提供还的释义，拼音[hái] [huán]，adv. also; still; even; yet； v. return; repay; give back； n. a surname； 网络释义： as well; too; in addition； " />
+
+ `美[ɡʊd]，英[ɡʊd]，
+  adv. 好； 
+  n. 好处；好人；益处；善行； 
+  adj. 有好处；好的；优质的；符合标准的； 
+  网络释义： 良好；很好；佳；
+  `
+   */
+  const description = $("meta[name=description]");
+  if (description.length > 0) {
+    const descriptionText = description.attr("content");
+    if (descriptionText) {
+      const arr = descriptionText.split("，");
+      if (arr.shift()) {
+        const paraphrase = arr.join("，");
+        console.warn(`Bing Paraphrase: ${paraphrase}`);
+      }
+    }
+  }
+}
+
 // parse word pronounce from html
 export function parsePhonetic(html: string) {
   const $ = cheerio.load(html);
 
   let phonetic;
+  // '美 [ɡʊd]'
   const pronounceText = $(".hd_p1_1>.hd_prUS").text();
+  console.warn(`pronounceText: ${pronounceText}`);
   if (pronounceText) {
     phonetic = pronounceText.split("[")[1].split("]")[0];
     console.warn(`phonetic: [${phonetic}]`);
