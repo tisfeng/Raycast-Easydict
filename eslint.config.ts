b5aa17f255451af587020ab34d4a3631c63a0a06
MaxChang3 <36927158+maxchang3@users.noreply.github.com>
2025-06-26 03:59:04 +0800

    feat: use flat ESLint configuration file

---
+++ eslint.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from "eslint/config";
+import raycastConfig from "@raycast/eslint-config";
+
+export default defineConfig([...raycastConfig, {
+    rules: {
+        "@typescript-eslint/no-unused-expressions": "off"
+    }
+}]);
