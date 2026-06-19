e6ddd322188f9749217d7731c4022289b0c1fb7f
MaxChang3 <36927158+maxchang3@users.noreply.github.com>
2026-06-19 19:35:42 +0800

    chore(eslint): setup import sort

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,4 +1,16 @@
-import { defineConfig } from "eslint/config";
 import raycastConfig from "@raycast/eslint-config";
+import { defineConfig } from "eslint/config";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
 
-export default defineConfig([...raycastConfig]);
+export default defineConfig([
+  ...raycastConfig,
+  {
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
+    rules: {
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+    },
+  },
+]);
